077d90ae9dd38a9f414d38cdd289e48f4c27202b
Frank <fha@easv.dk>
2024-08-29 11:28:00 +0200

    Add project files.

---
+++ WpfSimpleDataBinding/Model/Bindable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfSimpleDataBinding.Model
+{
+    abstract class Bindable : INotifyPropertyChanged
+    {
+        public void NotifyUI([CallerMemberName] String name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
---
+++ WpfSimpleDataBinding/Model/BmiInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfSimpleDataBinding.Model
+{
+    class BmiInfo : Bindable
+    {
+		//public int Height { get; set; }
+		private int height;
+
+		public int Height
+		{
+			get { return height; }
+			set { 
+				height = value;
+				//hej brugerflade læs mig igen
+				this.NotifyUI();
+			}
+		}
+
+		private int weight;
+
+		public int Weight
+		{
+			get { return weight; }
+			set { 
+				weight = value;
+				this.NotifyUI();
+			}
+		}
+
+	
+    }
+}
